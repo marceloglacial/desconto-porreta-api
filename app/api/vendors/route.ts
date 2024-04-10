79c9d53f9f5966811c91e2c2c39def2bd194f446
marceloglacial <glacial@gmail.com>
2024-04-10 00:42:40 +0000

    fix vendors

--- app/api/vendors/route.ts
+++ app/api/vendors/route.ts
@@ -6,13 +6,13 @@ const collection = 'vendors'
 export async function GET(_request: Request) {
     const client = await clientPromise
     const cursor = await client.db(database).collection(collection).find();
-    const products = (await cursor.toArray())
-    return Response.json(products)
+    const vendors = (await cursor.toArray())
+    return Response.json(vendors)
 }
 
 export async function POST(request: Request) {
     const client = await clientPromise;
     const body = await request.json()
-    await client.db(database).collection(collection).insertOne({ product: body.product });
+    await client.db(database).collection(collection).insertOne({ body });
     return Response.json({ message: "successfully updated the document" })
 }
