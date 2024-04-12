212671ea9373269d03de2cb9f20e0935be5d2380
marceloglacial <glacial@gmail.com>
2024-04-12 14:08:33 -0400

    fix error

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -24,20 +24,14 @@ export async function GET(_request: Request) {
 
 export async function POST(request: Request) {
     const client = await clientPromise;
-    try {
-        const body = await request.json()
-        const result = {
-            ...body,
-            vendor: ObjectId.createFromHexString(body.vendor)
-        }
+    const body = await request.json()
 
-        await client.db(database).collection(collection).insertOne(result);
-        const insertedId = result.insertedId;
-        return Response.json({ message: "successfully updated the document", id: insertedId })
-
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        await client.close(); // Close the connection
+    const result = {
+        ...body,
+        vendor: ObjectId.createFromHexString(body.vendor)
     }
+
+    await client.db(database).collection(collection).insertOne(result);
+    const insertedId = result.insertedId;
+    return Response.json({ message: "successfully updated the document", id: insertedId })
 }
