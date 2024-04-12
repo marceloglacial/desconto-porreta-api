68d469c3ba4ac245b5a418208f23d9db70113d72
marceloglacial <glacial@gmail.com>
2024-04-12 14:22:17 -0400

    fix response

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -26,12 +26,12 @@ export async function POST(request: Request) {
     const client = await clientPromise;
     const body = await request.json()
 
-    const result = {
+    const newProduct = {
         ...body,
         vendor: ObjectId.createFromHexString(body.vendor)
     }
 
-    await client.db(database).collection(collection).insertOne(result);
+    const result = await client.db(database).collection(collection).insertOne(newProduct);
     const insertedId = result.insertedId;
-    return Response.json({ message: "successfully updated the document", id: insertedId })
+    return Response.json({ message: "Successfully updated the document!", id: insertedId })
 }
