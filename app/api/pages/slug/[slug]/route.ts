6ec82179aaebf573addabe1254b0a6ac4e2e0179
marceloglacial <glacial@gmail.com>
2024-04-22 13:46:24 -0400

    add page by slug

---
+++ app/api/pages/slug/[slug]/route.ts
@@ -0,0 +1,29 @@
+import { COLLECTIONS, DATABASE_NAME } from '@/contants';
+import clientPromise from "@/services/mongodb";
+import { ObjectId } from 'mongodb';
+
+const database = DATABASE_NAME
+const collection = COLLECTIONS.PAGES
+
+export async function GET(_request: Request, { params }: { params: { slug: string } }
+) {
+    try {
+        const client = await clientPromise
+        const slug = params.slug
+        const query = { slug: slug };
+        const data = await client.db(database).collection(collection).findOne(query);
+        const result: IResponse = {
+            data,
+            status: 'success',
+            message: 'Success'
+        }
+        return Response.json(result)
+    } catch (e) {
+        const result: IResponse = {
+            data: null,
+            status: 'error',
+            message: 'Error: invalid page ID'
+        }
+        return Response.json(result)
+    }
+}
